764c2644a45d6dcc71fd11bb306b9c519acb4207
jcarlson <jake.carlson90@gmail.com>
2023-05-03 16:10:43 -0700

    update 1 5/3/23

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const colors = {
     '1': 'lime',
     '-1': 'pink',
@@ -14,8 +15,18 @@ const waysToWin = [
     [0,4,8]
     [2,4,6]
 ];
-
+//VARIBALES//
 let turn, canvas, winner
+
+//CACHED//
+const message = document.querySelector('h1');
+const playAgainBtn = document.querySelector('button');
+const canvasEls = [...document.querySelectorAll('#canvas > div ')];
+
+//EVENT LISTENERS//
+document.getElementById('canvas').addEventListener('click',
+handleMove);
+playAgainBtn.addEventListener('click', intialize)
 
 //FUNCTIONS//
 function announceWinner() {
