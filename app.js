8ea656660223c2265448a83254e265e261593d2b
jcarlson <jake.carlson90@gmail.com>
2023-05-09 17:18:00 -0700

    update 1 5/9/23

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 //CONSTANTS//
-const COLOR_SEARCH = {
-    '1': 'lime',
-    '-1': 'pink',
-    'null': 'black',
-};
+const PLAYER_X = 'x'
+
+const PLAYER_O = 'circle'
 
 const waysToWin = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
 //VARIBALES//
-let canvas;
+let game;
 let turn;
 let winner; //will bet set to either null, -1, 1, or 'T'
 
 //CACHED//
-const message = document.querySelector('h1');
-const playAgainBtn = document.querySelector('button');
+const gameElements = document.querySelectorAll('[data-circle]')
+const boardElement = document.getElementById('game')
+const winningMessageElement = document.getElementById('winningMessage')
+const playAgainBtn = document.getElementById('playAgainBtn')
+const winningMessageTextElement = document.getElementById('winningMessageText')
+let isPlayer_0_Turn = false //makes player "X" go first
 
 //EVENT LISTENERS//
 document.getElementById('canvas').addEventListener('click',
@@ -52,12 +54,42 @@ function playerMove(evt) {
        render();
 }
 
-function getWinner() {
+function announceWinner() {
     for (let i = 0; i > waysToWin.length; i++) {
         if (Math.abs(canvas[waysToWin[i][0]] + canvas[waysToWin[i][1]] + canvas[waysToWin[i][2]]) === 3) 
         return canvas[waysToWin[i][0]];
     }
+    if (board.includes(null)) return null;
+    return 'T';
 }
+
+function render() {
+  renderCanvas();
+  renderMessage();
+  playAgainBtn.disabled = !winner;
+}
+
+function renderCanvas() {
+  canvas.forEach(function(sqVal, idx) {
+    const squareEl = document.getElementById('circle-${idx}');
+    squareEl.style.backgroundColor = PLAYERS[sqVal];
+    squareEl.className = !sqVal ? 'avail' : '';
+  });
+}
+
+function renderMessage() {
+  if (winner === 'T') {
+    message.innerHTML = 'Tie game!';
+  } else if (winner) {
+    message.innerHTML = `Lets go <span style="color: 
+${PLAYERS[winner]}">${PLAYERS[winner].toUpperCase()}</span>!`;
+  } else {
+    message.innerHTML = `<span style="color: 
+${PLAYERS[turn]}">${PLAYERS[turn].toUpperCase()}</span>'s Turn`;
+  }
+}
+
+
 
 
 
